7979716b3193959aff8da50e8168dfbfcfe09063
jasen-devvv <janadevofficial@gmail.com>
2024-12-09 10:43:31 +0800

    feat: Add variable fonts (Quicksand, Lato) with custom colors and box shadow
    - Added Quicksand and Lato variable fonts with different font weights.
    - Defined custom color variables for primary, secondary, and background colors.
    
    - Configured custom box-shadow with transparency for specific elements.
    - Updated tailwind.config.js to include custom color and shadow settings.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,25 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'primary': '#092C4C',
+        'secondary': '#F2994A',
+        'info': '#2F80ED',
+        'success': '#27AE60',
+        'warning': '#FFD166',
+        'error': '#EB5757',
+        'black-1': '#000000',
+        'black-2': '#1D1D1D',
+        'black-3': '#282828',
+        'black-4': '#333333',
+        'white-1': '#FFFFFF',
+        'white-2': '#FAFAFA',
+        'white-3': '#F5F5F5',
+        'white-4': '#EEEEEE',
       },
+      boxShadow: {
+        'primary-shadow': '0 4px 6px rgba(168, 230, 207, 0.25)',
+        'black-shadow': '0 4px 6px rgba(0, 0, 0, 0.25)',
+      }
     },
   },
   plugins: [],
