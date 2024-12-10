e29a0f87779a284fe4bdbf9beae066f4d81adacb
jasen-devvv <janadevofficial@gmail.com>
2024-12-10 18:05:36 +0800

    feat: Add About page with React Icons (FaGithub and FaLinkedin)

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,3 +1,46 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa6"
+
 export default function About() {
-    return <h1>Hello About</h1>
+    const skills = [
+        {name: 'HTML'},
+        {name: 'CSS'},
+        {name: 'JAVASCRIPT'},
+        {name: 'PHP'},
+        {name: 'PYTHON'},
+        {name: 'GOLANG'},
+        {name: 'BOOTSTRAP'},
+        {name: 'TAILWINDCSS'},
+        {name: 'CODEIGNITER'},
+        {name: 'LARAVEL'},
+        {name: 'REACT'},
+        {name: 'VUE'},
+        {name: 'NEXT JS'},
+    ]
+
+    return (
+        <main className="w-full px-10">
+            <div className="text-center mt-5">
+                <h1>About</h1>
+                <p className="lato-regular opacity-70">A little bit about who I and my skills.</p>
+            </div>
+            <div className="flex justify-center mt-16 gap-x-16 w-full mx-auto max-w-[60rem]">
+                <img src="/img/logo.png" alt="Logo" className="rounded-full shadow-black-sb"/>
+                <div className="flex flex-col justify-between">
+                    <p className="text-lato-normal opacity-80">Hello! I'm Jana, a web developer passionate about creating interactive digital experience. With a strong foundation in design and full-stack technologies. I specialize in building responsive and accessible websites.</p>
+
+                    <h3>Skills</h3>
+                    <div className="flex flex-wrap gap-1">
+                        {skills.map((skill) => (
+                            <span key={skill.name} className="bg-teal-500 py-2 px-4 text-white-1 lato-bold text-lato-normal rounded-full">{skill.name}</span>
+                        ))}
+                    </div>
+                    <div className="flex gap-x-3">
+                        <button className="bg-violet-500 py-3 px-6 text-white-1 lato-bold text-lato-normal rounded-full">Download CV</button>
+                        <a className="bg-slate-900 p-3 text-white-1 lato-bold text-lato-normal rounded-full" href="#"><FaGithub className="text-3xl" /></a>
+                        <a className="bg-blue-500 p-3 text-white-1 lato-bold text-lato-normal rounded-full" href="#"><FaLinkedin className="text-3xl" /></a>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
 }
--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -2,13 +2,13 @@ import ParticlesLink from "@/components/ParticlesLink";
 
 export default function Home() {
   return (
-    <main className="h-svh">
+    <main>
       <ParticlesLink />
-      <div className="z-50 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="z-50 relative mt-32">
         <h1 className="text-black-1 text-center">JANA</h1>
-        <p className="text-center mb-5" style={{ 'fontFamily': 'var(--font-lato-regular);' }}>Full Stack Developer | Figma Designer</p>
-        <img src="/img/logo.png" alt="Logo" className="rounded-full object-cover max-w-full h-auto w-50 shadow-black-sb" />
-      </div>        
+        <p className="text-center mb-5 lato-regular opacity-70">Full Stack Developer | Figma Designer</p>
+        <img src="/img/logo.png" alt="Logo" className="rounded-full object-cover max-w-full mx-auto h-auto w-50 shadow-black-sb" />
+      </div>
     </main>
   );
 }
