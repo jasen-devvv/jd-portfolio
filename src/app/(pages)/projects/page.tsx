bf3229e61c6c9cc8e4e319494446fe0b5de872ad
jasen-devvv <janadevofficial@gmail.com>
2024-12-11 17:31:20 +0800

    feat: add responsive design for projects page and card components

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,70 +1,100 @@
-import Card from "@/components/Card"
+import Card from "@/components/Card";
 
 export default function Projects() {
-    const cardItems = [
-        {
-            title: 'JD - Blog',
-            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ab beatae quis corporis perferendis temporibus? Maxime illo perferendis repudiandae provident!',
-            tags: [
-                { name: 'Express' },
-                { name: 'React' },
-                { name: 'Tailwind' },
-                { name: 'MongoDB' },
-            ]
-        },
-        {
-            title: 'JD - Chat',
-            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ab beatae quis corporis perferendis temporibus? Maxime illo perferendis repudiandae provident!',
-            tags: [
-                { name: 'Golang' },
-                { name: 'Vue' },
-                { name: 'Tailwind' },
-                { name: 'MongoDB' },
-            ]
-        },
-        {
-            title: 'JD - TopUp',
-            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ab beatae quis corporis perferendis temporibus? Maxime illo perferendis repudiandae provident!',
-            tags: [
-                { name: 'Python' },
-                { name: 'Vue' },
-                { name: 'Tailwind' },
-                { name: 'MongoDB' },
-            ]
-        },
-        {
-            title: 'JD - Library',
-            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ab beatae quis corporis perferendis temporibus? Maxime illo perferendis repudiandae provident!',
-            tags: [
-                { name: 'Python' },
-                { name: 'Vue' },
-                { name: 'Tailwind' },
-                { name: 'MongoDB' },
-            ]
-        },
-        {
-            title: 'JD - Notes',
-            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ab beatae quis corporis perferendis temporibus? Maxime illo perferendis repudiandae provident!',
-            tags: [
-                { name: 'Golang' },
-                { name: 'Vue' },
-                { name: 'Tailwind' },
-                { name: 'MongoDB' },
-            ]
-        },
-    ]
+  const cardItems = [
+    {
+      title: "JD - Blog",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ab beatae quis corporis perferendis temporibus? Maxime illo perferendis repudiandae provident!",
+      tags: [
+        { name: "Express" },
+        { name: "React" },
+        { name: "Tailwind" },
+        { name: "MongoDB" },
+      ],
+      link: {
+        demo: '#',
+        code: '#'
+      }
+    },
+    {
+      title: "JD - Chat",
+      content:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid atque nihil id et obcaecati! Distinctio ea autem laudantium cumque provident harum. Neque labore veniam eaque recusandae quos exercitationem nobis ducimus odit ea delectus accusantium quo nemo, ut alias quisquam architecto amet repellendus! Expedita excepturi quae quod, necessitatibus mollitia natus facere!",
+      tags: [
+        { name: "Golang" },
+        { name: "Vue" },
+        { name: "Tailwind" },
+        { name: "MongoDB" },
+      ],
+      link: {
+        demo: '#',
+        code: '#'
+      }
+    },
+    {
+      title: "JD - TopUp",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ab beatae quis corporis perferendis temporibus? Maxime illo perferendis repudiandae provident!",
+      tags: [
+        { name: "Python" },
+        { name: "Vue" },
+        { name: "Tailwind" },
+        { name: "MongoDB" },
+      ],
+      link: {
+        demo: '#',
+        code: '#'
+      } 
+    },
+    {
+      title: "JD - Library",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ab beatae quis corporis perferendis temporibus? Maxime illo perferendis repudiandae provident!",
+      tags: [
+        { name: "Python" },
+        { name: "Vue" },
+        { name: "Tailwind" },
+        { name: "MongoDB" },
+      ],
+      link: {
+        demo: '#',
+        code: '#'
+      }
+    },
+    {
+      title: "JD - Notes",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ab beatae quis corporis perferendis temporibus? Maxime illo perferendis repudiandae provident!",
+      tags: [
+        { name: "Golang" },
+        { name: "Vue" },
+        { name: "Tailwind" },
+        { name: "MongoDB" },
+      ],
+      link: {
+        demo: '#',
+        code: '#'
+      }
+    },
+  ];
 
-    return (
-        <main className="w-full px-10">
-            <div className="text-center mt-5">
-                <h1>PROJECTS</h1>
-                <p className="lato-regular opacity-70">Here are some of the projects I've worked on recently.</p>
-            </div>
-            <div className="grid grid-cols-4 gap-4 mt-20">
-                {cardItems.map((data) => (
-                    <Card key={data.title} data={data}/>
-                ))}
-            </div>
-        </main>
-    )
+  return (
+    <main className="w-full px-5">
+      <div className="text-center mt-5">
+        <h1 className="text-h4 sm:text-h3 md:text-h2 lg:text-h1 font-quicksand">
+          PROJECTS
+        </h1>
+        <p className="opacity-50 font-latoRegular text-body-small sm:text-body-normal md:text-body-medium">
+          Here are some of the projects I've worked on recently.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 justify-items-center items-start md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 mt-16">
+        {cardItems.map((data) => (
+          <Card key={data.title} data={data} />
+        ))}
+      </div>
+    </main>
+  );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,32 +1,61 @@
 interface ITag {
-    name: string
+    name: string;
 }
 
 interface ICard {
     data: {
-        title: string,
-        content: string,
-        tags: ITag[]
-    }
+        title: string;
+        content: string;
+        tags: ITag[];
+        link: {
+            demo: string,
+            code: string
+        }
+    };
 }
 
-export default function Card({data}: ICard) {
+export default function Card({ data }: ICard) {
     return (
-        <div className="bg-blue-400 p-5 justify-between rounded-lg">
-            <img src="/img/logo.png" className="w-full h-60 object-cover" alt="Image" />
-            <div className="flex flex-col justify-between">
-                <p className="lato-bold text-lato-large">{data.title}</p>
-                <p className="lato-regular text-lato-small">{data.content}</p>
-                <div className="flex flex-wrap gap-1">
+        <div className="bg-white shadow-black-sb rounded-lg overflow-hidden w-full max-w-md">
+            <div className="relative">
+                <img
+                    src="/img/project.png"
+                    alt="Image"
+                    className="w-full h-60 object-cover transform hover:scale-105 transition-all duration-300 ease-in-out"
+                />
+            </div>
+
+            {/* Card Content */}
+            <div className="p-6">
+                <h3 className="text-3xl font-latoBold font-bold text-gray-800 truncate">{data.title}</h3>
+                <p className="text-black-1 font-latoRegular text-body-normal opacity-60 mt-3 text-base leading-relaxed line-clamp-3">{data.content}</p>
+
+                <div className="flex flex-wrap gap-2 mt-3">
                     {data.tags.map((tag) => (
-                        <span key={tag.name} className="bg-teal-500 py-1 px-2 my-2 text-white-1 lato-bold text-lato-normal rounded-full">{tag.name}</span>   
+                        <span
+                            key={tag.name}
+                            className="bg-primary shadow-primary-sb py-1 px-3 text-white-1 text-sm font-semibold rounded-full"
+                        >
+                            {tag.name}
+                        </span>
                     ))}
                 </div>
-                <div className="flex justify-between">
-                    <a href="#">Demo</a>
-                    <a href="#">View Code</a>
+
+                <div className="flex justify-between mt-4">
+                    <a
+                        href={data.link.code}
+                        className="text-secondary hover:text-teal-400 font-semibold transition duration-300 underline"
+                    >
+                        View Code
+                    </a>
+                    <a
+                        href={data.link.demo}
+                        className="text-secondary hover:text-teal-400 font-semibold transition duration-300 underline"
+                    >
+                        Demo
+                    </a>
                 </div>
             </div>
         </div>
-    )
+    );
 }
