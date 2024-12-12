67c1f19b38fdac138c8c903ee1f007347c774fcf
jasen-devvv <janadevofficial@gmail.com>
2024-12-12 14:06:46 +0800

    style: update contact layout and improve ui design responsive

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -2,21 +2,21 @@ import { FaEnvelope, FaFacebook, FaInstagram, FaLocationDot, FaPhone, FaTiktok, 
 
 export default function Contact() {
   return (
-    <main className="w-full px-10">
+    <main className="w-full px-5 sm:px-10 md:px-20">
       <div className="text-center mt-5">
-        <h1>CONTACT</h1>
-        <p className="lato-regular opacity-70">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-quicksand font-semibold">
+          CONTACT
+        </h1>
+        <p className="opacity-70 font-latoRegular text-sm sm:text-base md:text-lg mt-2">
           I'd love to hear from you! <br />
-          Whetever it's a project, job oppurtunity or just a chat.
+          Whether it's a project, job opportunity, or just a chat.
         </p>
       </div>
 
-      <div className="flex justify-center gap-x-5 mt-16">
-        <div className="bg-green-400 p-3 w-[36rem] rounded-xl shadow-black-sb text-center">
-          <h4>FORM CONTACT</h4>
-          <p className="lato-regular text-lato-normal opacity-70">
-            Send me a message!
-          </p>
+      <div className="flex flex-col sm:flex-row justify-center items-start gap-10 mt-16">
+        <div className="bg-primary p-6 w-full sm:w-[36rem] rounded-xl shadow-black-sb">
+          <h4 className="text-h6 md:text-h5 lg:text-h4 font-quicksand font-bold text-black-1 text-center">CONTACT FORM</h4>
+          <p className="font-latoRegular text-body-normal opacity-60 text-center mt-2">Send me a message!</p>
 
           <form method="POST" className="mt-10">
             <div className="mb-4">
@@ -35,38 +35,48 @@ export default function Contact() {
               <label htmlFor="message" className="block text-gray-700 text-left font-medium lato-bold text-lato-medium mb-2">Your Message</label>
               <textarea placeholder="Message here..." id="message" className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-blue-500 h-32"></textarea>
             </div>
-            <div className="mb-4">
-                <button className="bg-red-500 py-2 px-4 text-white lato-bold text-lato-large rounded-lg">Submit</button>
+            <div className="mb-4 text-center">
+              <button className="bg-info py-2 px-6 text-white font-bold text-lg rounded-lg hover:bg-red-600 transition duration-300 ease-in-out">Submit</button>
             </div>
           </form>
         </div>
-        <div className="flex flex-col justify-between">
+
+        <div className="w-full sm:w-[36rem] flex flex-col gap-6">
           <div>
-            <h5>Contact Information</h5>
-            <p className="lato-regular text-lato-normal opacity-70">
-              Feel free to get in touch with me via email or phone.
-            </p>
+            <h5 className="text-h5 md:text-h4 lg:text-h3 font-quicksand font-bold">Contact Information</h5>
+            <p className="font-latoRegular text-body-small md:text-body-normal lg:text-body-medium opacity-60">Feel free to get in touch with me via email or phone.</p>
           </div>
 
-          <div className="mt-5">
-            <p className="flex gap-x-1"><FaLocationDot /><span className="text-blue-500 lato-bold">Address</span>: Jln. Berangas Barat RT. 7 RW. 3, Kecamatan Barito Kuala, Kalimantan Selatan</p>
-            <p className="flex gap-x-1"><FaEnvelope /><span className="text-blue-500 lato-bold">Email</span>: janadevofficial@gmail.com</p>
-            <p className="flex gap-x-1"><FaPhone /><span className="text-blue-500 lato-bold">Telphone</span>: (+62) 887 - 0527 - 9826</p>
+          <div className="space-y-4">
+            <p className="flex gap-x-2 font-latoRegular items-center"><FaLocationDot color="#FF0000" className="flex-shrink-0" /><div><span className="text-blue-500 font-latoBold font-bold">Address:</span> Jln. Berangas Barat RT. 7 RW. 3, Kecamatan Barito Kuala, Kalimantan Selatan</div></p>
+            <p className="flex gap-x-2 font-latoRegular items-center"><FaEnvelope color="#6C757D" className="flex-shrink-0" /><div><span className="text-blue-500 font-latoBold font-bold">Email:</span> janadevofficial@gmail.com</div></p>
+            <p className="flex gap-x-2 font-latoRegular items-center"><FaPhone color="#4CAF50" className="flex-shrink-0" /><div><span className="text-blue-500 font-latoBold font-bold">Phone:</span> (+62) 887 - 0527 - 9826</div></p>
           </div>
 
-          <div className="mt-5">
-            <h5>Follow Me</h5>
-            <ul className="flex flex-wrap my-2 gap-x-2">
-              <li className="bg-red-500 p-2 rounded-full text-white-1"><a href="#"><FaInstagram className="text-4xl" /></a></li>
-              <li className="bg-red-500 p-2 rounded-full text-white-1"><a href="#"><FaFacebook className="text-4xl" /></a></li>
-              <li className="bg-red-500 p-2 rounded-full text-white-1"><a href="#"><FaTwitter className="text-4xl" /></a></li>
-              <li className="bg-red-500 p-2 rounded-full text-white-1"><a href="#"><FaTiktok className="text-4xl" /></a></li>
-              <li className="bg-red-500 p-2 rounded-full text-white-1"><a href="#"><FaYoutube className="text-4xl" /></a></li>
+          <div>
+            <h5 className="font-quicksand text-h6 md:text-h5 lg:text-h4 font-bold">Follow Me</h5>
+            <ul className="flex flex-wrap gap-4">
+              <li className="bg-white-1 p-3 rounded-full hover:bg-white-4 transition duration-300 ease-in-out shadow-black-sb">
+                <a href="#"><FaInstagram color="#E1306C" className="text-3xl" /></a>
+              </li>
+              <li className="bg-white-1 p-3 rounded-full hover:bg-white-4 transition duration-300 ease-in-out shadow-black-sb">
+                <a href="#"><FaFacebook color="#1877F2" className="text-3xl" /></a>
+              </li>
+              <li className="bg-white-1 p-3 rounded-full hover:bg-white-4 transition duration-300 ease-in-out shadow-black-sb">
+                <a href="#"><FaTwitter color="#1DA1F2" className="text-3xl" /></a>
+              </li>
+              <li className="bg-black-1 p-3 rounded-full hover:bg-black-4 transition duration-300 ease-in-out shadow-black-sb">
+                <a href="#"><FaTiktok color="#FFFFFF" className="text-3xl" /></a>
+              </li>
+              <li className="bg-white-1 p-3 rounded-full hover:bg-white-4 transition duration-300 ease-in-out shadow-black-sb">
+                <a href="#"><FaYoutube color="#FF0000" className="text-3xl" /></a>
+              </li>
             </ul>
           </div>
+
           <div>
-            <h5>My Location</h5>
-            <iframe className="mt-5 rounded-xl shadow-black-sb" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d248.9615518325915!2d114.56781463470155!3d-3.2541891633074256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sid!4v1733839188022!5m2!1sen!2sid" width="600" height="350" style={{'border': '0px'}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            <h5 className="font-quicksand text-h6 md:text-h5 lg:text-h4 font-bold">My Location</h5>
+            <iframe className="mt-5 rounded-xl shadow-black-sb" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d248.9615518325915!2d114.56781463470155!3d-3.2541891633074256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sid!4v1733839188022!5m2!1sen!2sid" width="100%" height="350" style={{'border': '0'}} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </div>
