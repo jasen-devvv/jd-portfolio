b46391bfc7c4772ddfa33096c5b15be1cc379b03
jasen-devvv <janadevofficial@gmail.com>
2024-12-12 13:06:57 +0800

    style: update Timeline layout and improve UI design

--- src/app/(pages)/timeline/page.tsx
+++ src/app/(pages)/timeline/page.tsx
@@ -5,29 +5,36 @@ export default function Timeline() {
     <main className="w-full px-5">
       <div className="text-center mt-5">
         <h1 className="text-h4 sm:text-h3 md:text-h2 lg:text-h1 font-quicksand">
-        TIMELINE
+          TIMELINE
         </h1>
         <p className="opacity-50 font-latoRegular text-body-small sm:text-body-normal md:text-body-medium">
-        A journey through my education and career.
+          A journey through my education and career.
         </p>
       </div>
+
       <div className="flex items-center justify-center mt-16 bg-white-1 shadow-black-sb py-4 rounded-lg px-6">
-        <div className="space-y-10 border-l-2 border-dashed">
-          {timelineItems.map((timeline) => (
-            <div key={timeline.title} className="relative w-full">
-              {timeline.icon}
-              <div className="ml-6">
-                <h4 className="font-bold font-quicksand text-black-1">{timeline.title}</h4>
-                <p className="mt-2 max-w-screen-sm font-latoRegular text-sm text-gray-500">
-                  {timeline.description}
-                </p>
-                <span className="mt-1 block text-sm font-semibold text-blue-500">
-                  {timeline.year}
-                </span>
+        <ol>
+          {timelineItems.map((timeline, index) => (
+            <li key={index} className="border-l-2 border-primary">
+              <div className="flex justify-start">
+                <div className="bg-primary w-10 h-10 p-2 flex items-center justify-center rounded-full -ml-5">
+                  {timeline.icon}
+                </div>
+                <div className="block p-6 rounded-lg shadow-lg bg-gray-100 max-w-lg ml-6 mb-10">
+                  <div className="flex justify-between mb-4">
+                    <p className="font-quicksand font-bold text-black-1 hover:text-black-2 focus:text-black-3 duration-300 transition ease-in-out text-sm">
+                      {timeline.title}
+                    </p>
+                    <p className="font-quicksand font-bold text-black-1 hover:text-black-2 focus:text-black-3 duration-300 transition ease-in-out text-sm">
+                      {timeline.year}
+                    </p>
+                  </div>
+                  <p className="text-black-1 font-latoRegular text-body-normal opacity-70 mb-6">{timeline.description}</p>
+                </div>
               </div>
-            </div>
+            </li>
           ))}
-        </div>  
+        </ol>
       </div>
     </main>
   );
