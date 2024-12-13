3c976f5886a83fb83f06fde93812281d67351218
jasen-devvv <janadevofficial@gmail.com>
2024-12-13 18:12:23 +0800

    feat: add enter animation and add id
    - Animation title & description
    - Add ID at data/skillItems.tsx

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Loading from "@/app/loading";
 import skills from "@/data/skillItems";
-import { Suspense } from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa6";
 import { motion } from "motion/react";
 
@@ -28,14 +26,25 @@ const childVariants = {
 export default function About() {
   return (
     <main className="w-full px-5">
-      <div className="text-center mt-5">
-        <h1 className="text-h4 sm:text-h3 md:text-h2 lg:text-h1 font-quicksand">
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        className="text-center mt-5"
+      >
+        <motion.h1
+          variants={childVariants}
+          className="text-h4 sm:text-h3 md:text-h2 lg:text-h1 font-quicksand"
+        >
           ABOUT
-        </h1>
-        <p className="opacity-50 font-latoRegular text-body-small sm:text-body-normal md:text-body-medium">
-          A little bit about who I and my skills.
-        </p>
-      </div>
+        </motion.h1>
+        <motion.p
+          variants={childVariants}
+          className="font-latoRegular text-body-small sm:text-body-normal md:text-body-medium"
+        >
+          <span className="opacity-50">A little bit about who I and my skills.</span>
+        </motion.p>
+      </motion.div>
       <div className="flex flex-col lg:flex-row justify-center mt-16 gap-x-16 mx-auto max-w-[60rem]">
         <motion.img
           variants={childVariants}
@@ -72,10 +81,10 @@ export default function About() {
             animate="visible"
             className="flex flex-wrap gap-2 mt-2"
           >
-            {skills.map((skill, index) => (
+            {skills.map((skill) => (
               <motion.span
                 variants={childVariants}
-                key={index}
+                key={skill.id}
                 className="bg-white-2 py-2 px-4 text-black-1 font-latoBold text-body-normal rounded-full shadow-black-sb flex items-center gap-x-1"
               >
                 {skill.icon}
--- src/data/skillItems.tsx
+++ src/data/skillItems.tsx
@@ -14,22 +14,23 @@ import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
 import { SiPhp } from "react-icons/si";
 
 const skillsItems = [
-  { name: "HTML", icon: <FaHtml5 color="#E34F26" size={24} /> },
-  { name: "CSS", icon: <FaCss3Alt color="#1572B6" size={24} /> },
-  { name: "JAVASCRIPT", icon: <FaJs color="#F7DF1E" size={24} /> },
-  { name: "PHP", icon: <SiPhp color="#777BB4" size={24} /> },
-  { name: "PYTHON", icon: <FaPython color="#3776AB" size={24} /> },
-  { name: "GOLANG", icon: <FaGolang color="#00ADD8" size={24} /> },
-  { name: "BOOTSTRAP", icon: <FaBootstrap color="#7952B3" size={24} /> },
+  { id: 1, name: "HTML", icon: <FaHtml5 color="#E34F26" size={24} /> },
+  { id: 2, name: "CSS", icon: <FaCss3Alt color="#1572B6" size={24} /> },
+  { id: 3, name: "JAVASCRIPT", icon: <FaJs color="#F7DF1E" size={24} /> },
+  { id: 4, name: "PHP", icon: <SiPhp color="#777BB4" size={24} /> },
+  { id: 5, name: "PYTHON", icon: <FaPython color="#3776AB" size={24} /> },
+  { id: 6, name: "GOLANG", icon: <FaGolang color="#00ADD8" size={24} /> },
+  { id: 7, name: "BOOTSTRAP", icon: <FaBootstrap color="#7952B3" size={24} /> },
   {
+    id: 8,
     name: "TAILWINDCSS",
     icon: <RiTailwindCssFill color="#06B6D4" size={24} />,
   },
-  { name: "CODEIGNITER", icon: <DiCodeigniter color="#EF4223" size={24} /> },
-  { name: "LARAVEL", icon: <FaLaravel color="#FF2D20" size={24} /> },
-  { name: "REACT", icon: <FaReact color="#61DAFB" size={24} /> },
-  { name: "VUE", icon: <FaVuejs color="#41B883" size={24} /> },
-  { name: "NEXT JS", icon: <RiNextjsFill color="#000000" size={24} /> },
+  { id: 9, name: "CODEIGNITER", icon: <DiCodeigniter color="#EF4223" size={24} /> },
+  { id: 10, name: "LARAVEL", icon: <FaLaravel color="#FF2D20" size={24} /> },
+  { id: 11, name: "REACT", icon: <FaReact color="#61DAFB" size={24} /> },
+  { id: 12, name: "VUE", icon: <FaVuejs color="#41B883" size={24} /> },
+  { id: 13, name: "NEXT JS", icon: <RiNextjsFill color="#000000" size={24} /> },
 ];
 
 export default skillsItems;
